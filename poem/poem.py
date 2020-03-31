0a25167c10869d5b4f50462dac6ef72ae0f07160
高亮 <gaoliang.0708@bytedance.com>
2020-03-31 15:10:14 +0800

    update
    Change-Id: I6613848bcc9e9aeaf9922da00ce1483b184cd6c1

--- poem/poem.py
+++ poem/poem.py
@@ -1,59 +1,37 @@
 # coding: UTF-8
-import socket
-import urllib.request
-import re
+import urllib3
 import random
-import urllib.error
+import sys
+from bs4 import BeautifulSoup, Tag, NavigableString
+http = urllib3.PoolManager()
 
-base_url = "http://so.gushiwen.org/view_{0}.aspx".format(random.randint(1,72866))
-# base_url = "http://so.gushiwen.org/view_20914.aspx"
-# base_url = "http://so.gushiwen.org/view_5512.aspx"
-try:
-    f = urllib.request.urlopen(base_url, timeout=3)
+while True:
+    base_url = "http://so.gushiwen.org/view_{0}.aspx".format(
+        random.randint(1, 72866))
+    # base_url = "http://so.gushiwen.org/view_20914.aspx"
+    # base_url = "http://so.gushiwen.org/view_5512.aspx"
+    try:
+        f = http.request('GET', base_url, timeout=4.0)
+    except:
+        print("timeout: " + base_url)
+        sys.exit(-1)
 
-    line = ""
-    currentLine = 0
-    while True:
-        line = f.readline().decode("UTF-8")
-        currentLine += 1
-        if re.search('shileft', line):
-            break
-        if currentLine > 3000:
-            print(base_url)
-            exit(-1)
+    soup = BeautifulSoup(f.data, 'lxml')
+    poem = soup.find('div', 'main3')
+    if poem is not None:
+        break
+total_content = poem.find('div', 'left').find('div',
+                                              'sons').find('div', 'cont')
+title = total_content.find('h1').string
 
-    f.readline()
-    poem_name = re.search('<h1>([^<]*)</h1>', f.readline().decode("UTF-8")).group(1)
-    print('\33[5;36m'+poem_name)
+author_info = total_content.find('p', 'source').find_all('a')
+dynasty = author_info[0].contents[0]
+author_name = author_info[1].contents[0]
 
-    while True:
-        line = f.readline().decode("UTF-8")
-        if re.search('朝代', line):
-            break
-    generation = re.search("([\u4e00-\u9fa5]*)</p>", line).group(1)
-    poet_name = re.search("([\u4e00-\u9fa5]*)(</a>)?</p>", f.readline().decode("UTF-8")).group(1)
-    print("["+generation+"]•" + poet_name)
-
-    f.readline()
-    brElement = re.compile("<br ?/>")
-    pElement = re.compile("</?p>")
-    while True:
-        line = f.readline().decode("UTF-8").strip()
-        line = re.sub(brElement, "", line)
-        if re.search("[\u4e00-\u9fa5]", line):
-            line = re.sub(pElement, "", line)
-            print(line.replace("。", "。\n").replace(".", "。\n"), end='')
-        elif len(line) == 0:
-            continue
-        elif re.search('</div>', line):
-            break
-        else:
-            print(line)
-            print("hello")
-            break
-    print('\033[0m')
-except urllib.error.URLError as e:
-    print(base_url)
-    print(e.reason)
-except socket.timeout as e:
-    print(base_url + ": time out")
+content = total_content.find('div', 'contson').contents
+print('\33[96m' + title)
+print("[" + dynasty + "]•" + author_name)
+for item in content:
+    if isinstance(item, NavigableString):
+        print(item.replace("\n", ""))
+print('\033[0m')
